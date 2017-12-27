38f3a6426f0f4a2a935c3cf19451969ea91d8297
ItsJohnCena <kristy.crazy4pandas.gao@gmail.com>
2017-12-27 12:23:21 -0500

    testing

--- scripts/structure.js
+++ scripts/structure.js
@@ -64,7 +64,7 @@ function arrayLoad(){
 
 //quiz loading
 $(window).on('load', function () {
-    console.log("nice");
+    console.log("heyo");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {//when the readystate changes
        // alert("readyState " + this.readyState + " stats " + this.status );   
